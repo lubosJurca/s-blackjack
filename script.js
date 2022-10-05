b4b06402641d9169076c966d7312cd9ad7d60bab
Lubos Jurca <lubos.jurca@gmail.com>
2022-10-05 11:54:08 +0200

    upravy v css a js

--- script.js
+++ script.js
@@ -4,15 +4,26 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let myCards = document.getElementById("myCards")
+let score = document.getElementById("score")
+let actionText = document.getElementById("message")
 
-if (sum < 21) {
-    message = "Do you want to draw a new card?"
-} else if (sum === 21) {
-    message = "Wohoo! You have got Blackjack!"
-    hasBlackJack = true
-} else {
-    message = "You are out of the game!"
-    isAlive = false
+function startGame() {
+    if (sum < 21) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Wohoo! You have got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You are out of the game!"
+        isAlive = false
+    }
+
+    myCards.textContent += ` ${firstCard} and ${secondCard}`
+    score.textContent += ` ${sum}`
+    actionText.textContent = message
+
+
+
 }
 
-console.log(message)
